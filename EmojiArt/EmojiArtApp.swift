f1edc600d7c0a1862e8fc8e693bfcb69ea6f214e
krit sukrit <sukrit@dek-d.com>
2022-01-28 13:22:23 +0700

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Sukrit Chatmeeboon on 28/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
