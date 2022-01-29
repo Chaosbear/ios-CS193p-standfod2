213402fec395b4a3517e8770cfa968cbc9fe47e1
krit sukrit <sukrit@dek-d.com>
2022-01-29 17:30:16 +0700

    initial commit at lecture10

---
+++ EmojiArt/UtilityViews.swift
@@ -0,0 +1,46 @@
+//
+//  utilityViews.swift
+//  EmojiArt
+//
+//  Created by Sukrit Chatmeeboon on 28/1/2565 BE.
+//
+
+import SwiftUI
+
+struct OptionalImage: View {
+    var uiImage: UIImage?
+    
+    var body: some View {
+        if uiImage != nil {
+            Image(uiImage: uiImage!)
+        }
+    }
+}
+
+struct AnimatedActionButton: View {
+    var title: String? = nil
+    var systemImage: String? = nil
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            withAnimation{
+                action()
+                
+            }
+        } label: {
+            if title != nil && systemImage != nil {
+                Label(title!, systemImage: systemImage!)
+            } else if title != nil {
+                Text(title!)
+            } else if systemImage != nil {
+                Image(systemName: systemImage!)
+            }
+        }
+    }
+}
+
+struct IdentifiableAlert: Identifiable {
+    var id: String
+    var alert:  () -> Alert
+}
