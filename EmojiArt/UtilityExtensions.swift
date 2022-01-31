fc04e44c7758f1f9e83907f7c063c0ecf1f73a32
krit sukrit <sukrit@dek-d.com>
2022-01-31 23:10:09 +0700

    commit at lecture12

--- EmojiArt/UtilityExtensions.swift
+++ EmojiArt/UtilityExtensions.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 extension Collection where Element: Identifiable {
     func index(matching element: Element) -> Self.Index? {
-        firstIndex(where: {$0.id == element.id})
+        firstIndex(where: { $0.id == element.id })
     }
 }
+
+// we could do the same thing when it comes to removing an element
+// but we have to add that to a different protocol
+// because Collection works for immutable collections of things
+// the "mutable" one is RangeReplaceableCollection
+// not only could we add remove
+// but we could add a subscript which takes a copy of one of the elements
+// and uses its Identifiable-ness to subscript into the Collection
+// this is an awesome way to create Bindings into an Array in a ViewModel
+// (since any Published var in an ObservableObject can be bound to via $)
+// (even vars on that Published var or subscripts on that var)
+// (or subscripts on vars on that var, etc.)
 
 extension RangeReplaceableCollection where Element: Identifiable {
     mutating func remove(_ element: Element) {
@@ -19,7 +31,7 @@ extension RangeReplaceableCollection where Element: Identifiable {
             remove(at: index)
         }
     }
-    
+
     subscript(_ element: Element) -> Element {
         get {
             if let index = index(matching: element) {
@@ -36,6 +48,9 @@ extension RangeReplaceableCollection where Element: Identifiable {
     }
 }
 
+// if you use a Set to represent the selection of emoji in HW5
+// then you might find this syntactic sugar function to be of use
+
 extension Set where Element: Identifiable {
     mutating func toggleMembership(of element: Element) {
         if let index = index(matching: element) {
@@ -46,15 +61,20 @@ extension Set where Element: Identifiable {
     }
 }
 
+// some extensions to String and Character
+// to help us with managing our Strings of emojis
+// we want them to be "emoji only"
+// (thus isEmoji below)
+// and we don't want them to have repeated emojis
+// (thus withoutDuplicateCharacters below)
+
 extension String {
-    var withNoRepeatedCharacters: String {
-        var uniqued = ""
-        for ch in self {
-            if !uniqued.contains(ch) {
-                uniqued.append(ch)
+    var removingDuplicateCharacters: String {
+        reduce(into: "") { sofar, element in
+            if !sofar.contains(element) {
+                sofar.append(element)
             }
         }
-        return uniqued
     }
 }
 
--- EmojiArt/UtilityViews.swift
+++ EmojiArt/UtilityViews.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+// syntactic sure to be able to pass an optional UIImage to Image
+// (normally it would only take a non-optional UIImage)
+
 struct OptionalImage: View {
     var uiImage: UIImage?
     
@@ -17,6 +20,14 @@ struct OptionalImage: View {
     }
 }
 
+// syntactic sugar
+// lots of times we want a simple button
+// with just text or a label or a systemImage
+// but we want the action it performs to be animated
+// (i.e. withAnimation)
+// this just makes it easy to create such a button
+// and thus cleans up our code
+
 struct AnimatedActionButton: View {
     var title: String? = nil
     var systemImage: String? = nil
@@ -24,9 +35,8 @@ struct AnimatedActionButton: View {
     
     var body: some View {
         Button {
-            withAnimation{
+            withAnimation {
                 action()
-                
             }
         } label: {
             if title != nil && systemImage != nil {
@@ -40,7 +50,15 @@ struct AnimatedActionButton: View {
     }
 }
 
+// simple struct to make it easier to show configurable Alerts
+// just an Identifiable struct that can create an Alert on demand
+// use .alert(item: $alertToShow) { theIdentifiableAlert in ... }
+// where alertToShow is a Binding<IdentifiableAlert>?
+// then any time you want to show an alert
+// just set alertToShow = IdentifiableAlert(id: "my alert") { Alert(title: ...) }
+// of course, the string identifier has to be unique for all your different kinds of alerts
+
 struct IdentifiableAlert: Identifiable {
     var id: String
-    var alert:  () -> Alert
+    var alert: () -> Alert
 }
